72bf1af5e09b36eb9b54e3052b022ae570b7e7b4
Dieg0Ch4ves <diegoaraujo200570@gmail.com>
2023-06-23 10:15:01 -0400

    Update projetos

--- src/app/projetos/projetos.component.ts
+++ src/app/projetos/projetos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-projetos',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./projetos.component.css']
 })
 export class ProjetosComponent {
+
+  constructor(private router: Router) { }
+
+  abrir(url: string) {
+   window.open(url, '_blank');
+  }
 
 }
